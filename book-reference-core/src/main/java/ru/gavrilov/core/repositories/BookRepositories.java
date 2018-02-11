0e8d8b0325561213dac7e4cd11ab39e944679967
Gavrilov Ivan <ivan.gavrilov@informicus.ru>
2018-02-11 20:46:49 +0300

    добавление нескольких запросов в BookRepositories.java

--- book-reference-core/src/main/java/ru/gavrilov/core/repositories/BookRepositories.java
+++ book-reference-core/src/main/java/ru/gavrilov/core/repositories/BookRepositories.java
@@ -3,8 +3,17 @@ package ru.gavrilov.core.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import ru.gavrilov.core.authors.model.Author;
 import ru.gavrilov.core.books.model.Book;
+import ru.gavrilov.core.users.model.User;
+
+import java.util.List;
 
 
 public interface BookRepositories extends JpaRepository<Book,Long>, JpaSpecificationExecutor<Book> {
+
+    List<Book> findByAuthorAndUser(Author author, User user);
+    List<Book> findByAuthor(Author author);
+    List<Book> findByUser(User user);
+
 }
